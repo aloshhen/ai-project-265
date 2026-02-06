c0adc8f3ce0ebbf99df85cbad94a9a42145b9c54
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-06 14:05:43 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+}
